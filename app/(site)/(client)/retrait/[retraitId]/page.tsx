104b0b186a96069bfb283f5f02927de978248e97
wilfriedmoutsig <34437117-wilfriedmoutsig@users.noreply.replit.com>
2024-07-07 23:31:29 +0000

    Hide getCurrentUser in retraitId

--- app/(site)/(client)/retrait/[retraitId]/page.tsx
+++ app/(site)/(client)/retrait/[retraitId]/page.tsx
@@ -2,7 +2,7 @@ import { CreditMenu } from "@/features/site/client/credit-menu";
 import { DashboardInfo } from "@/features/site/client/dashboard-info";
 
 //import { auth } from "@/auth";
-import { getCurrentUser } from "@/data/current-user";
+import { validateRequest } from "@/data/current-user";
 import { getLastWithDraw } from "@/data/user";
 
 import { userTotalCredit } from "@/data/user";
@@ -12,8 +12,8 @@ interface RetraitPageProps {
 }
 
 export default async function RetraitPage({ params } : RetraitPageProps ) {
-  const session = await getCurrentUser();
-  const all_credit = session ? await userTotalCredit(session.id) : null;
+  const { user } = await validateRequest();
+  const all_credit = user ? await userTotalCredit(user?.id) : null;
   console.log('all_credit', all_credit);
 
   const lastQuantity = await getLastWithDraw(all_credit?.id);
@@ -21,12 +21,12 @@ export default async function RetraitPage({ params } : RetraitPageProps ) {
   return (
     <div className="flex flex-col py-0.5">
       
-      <DashboardInfo all_credit={all_credit} session={session} />
+      <DashboardInfo all_credit={all_credit} session={user} />
       
       <section className="w-full flex flex-col items-center px-4 py-2 space-y-4">
         <h1 className="w-full flex items-center text-gray-200 text-md font-[600] gap-2">Crédit</h1>
         <div className="w-full grid grid-cols-1 gap-2.5">
-          <CreditMenu all_credit={all_credit} session={session} lastQuantity={lastQuantity} />
+          <CreditMenu all_credit={all_credit} session={user} lastQuantity={lastQuantity} />
         </div>
       </section>
       
